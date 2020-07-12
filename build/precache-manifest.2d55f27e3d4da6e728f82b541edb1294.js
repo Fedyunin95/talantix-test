e49b2b0a9912695227974691d1efaec48604dc77
Fedyunin95 <fedyunin.yasher@gmail.com>
2020-07-12 23:48:15 +0200

    Publish build

---
+++ build/precache-manifest.2d55f27e3d4da6e728f82b541edb1294.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f748d32e1f2f1b313a53652d1d233d26",
+    "url": "/talantix-test/index.html"
+  },
+  {
+    "revision": "dd078332cdf263460db9",
+    "url": "/talantix-test/static/css/main.f73358fb.chunk.css"
+  },
+  {
+    "revision": "e0b8fc57bf96f2cddea2",
+    "url": "/talantix-test/static/js/2.d1f38f84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/talantix-test/static/js/2.d1f38f84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd078332cdf263460db9",
+    "url": "/talantix-test/static/js/main.e3959d81.chunk.js"
+  },
+  {
+    "revision": "3f40f179a7e7b929b668",
+    "url": "/talantix-test/static/js/runtime-main.30f3c314.js"
+  }
+]);
